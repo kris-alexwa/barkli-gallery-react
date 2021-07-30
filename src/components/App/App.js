5570aab50f8817aea38c4ab71912f66d320d2512
Kris <kris_alexwa@mail.ru>
2021-07-30 21:59:26 +0300

    сверстала header, подключила шрифты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,15 @@
-
+import React from 'react';
 import './App.css';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <>
+    <body className='body'>
+      <div className='page'>
+        <Header />
+      </div>
+    </body>
     </>
   )
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+import burgerMenu from '../../images/icons/burger-menu-icon.svg';
+
+function Header() {
+    return (
+        <>
+            <header className='header'>
+                <img src={burgerMenu} className='header__burger-menu-icon' alt='Иконка бургер-меню'/>
+                <p className='header__phone-number'>&#43;7&#32;495&#32;320&#x2010;02&#x2010;98</p>
+            </header>
+        </>
+    )
+}
+
+export default Header;
